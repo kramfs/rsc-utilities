99216cbad89384cda27ea3bb38b7784b184e3fc6
Rudel Saldivar <rudelsaldivar@gmail.com>
2015-08-05 19:10:24 +0800

    script will be backup in rightscript_backup dir

--- backup_rightscripts/backup_rightscripts.sh
+++ backup_rightscripts/backup_rightscripts.sh
@@ -1,22 +1,26 @@
 #!/bin/bash -e
 # Backup all rightscripts in the account
+#ACCOUNT=12345
 ACCOUNT=
+#SHARD=my.rightscale.com or us-4.rightscale.com
 SHARD=
 REFRESHTOKEN= 
 EMAIL=
-PWD=
+PASSWORD=
 
 ## Uncomment and use this when using EMAIL/PASSWORD credential
-source_href=`/usr/local/bin/rsc --pp -a $ACCOUNT --email=$EMAIL --pwd=$PWD -h $SHARD --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts |sed -e s/\"//g`
+source_href=`/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts |sed -e s/\"//g`
 
 ## Uncomment and use this when using REFRESH TOKEN.
-# source_href=`/usr/local/bin/rsc --pp -a $ACCOUNT -h $SHARD --refreshToken=$REFRESHTOKEN --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts |sed -e s/\"//g`
+# source_href=`/usr/local/bin/rsc --pp -a "$ACCOUNT" -h "$SHARD" --refreshToken="$REFRESHTOKEN" --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts |sed -e s/\"//g`
 
 ## Backup only specific rightscripts filter by "name 
-# source_href=`/usr/local/bin/rsc --pp -a $ACCOUNT -h $SHARD --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts filter[]="name==rjs:" |sed -e s/\"//g`
+# source_href=`/usr/local/bin/rsc --pp -a "$ACCOUNT" -h "$SHARD" --pwd="$PASSWORD" -h "$SHARD" --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts filter[]="name==rjs:" |sed -e s/\"//g`
 
+mkdir -p rightscript_backup
+cd rightscript_backup
 for i in $source_href ;
 do
-	script_name=`/usr/local/bin/rsc --pp -a $ACCOUNT -h $SHARD --email=$EMAIL --pwd=$PWD --x1=.name cm15 show $i`
-	/usr/local/bin/rsc --pp -a $ACCOUNT -h $SHARD --email=$EMAIL --pwd=$PWD cm15 show_source $i > "$script_name"
+	script_name=`/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" --x1=.name cm15 show $i`
+	/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" cm15 show_source $i > "$script_name"
 done
