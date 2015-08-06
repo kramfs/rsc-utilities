8995d1fc9c21fd6d1c4c9dce6d5d7f11399789ad
Rudel Saldivar <rudelsaldivar@gmail.com>
2015-08-06 13:13:16 +0800

    Added logging, save backup script in each folder

--- backup_rightscripts/backup_rightscripts.sh
+++ backup_rightscripts/backup_rightscripts.sh
@@ -20,13 +20,17 @@ source_href=`/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASS
 ## Backup only specific rightscripts filter by "name 
 # source_href=`/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" --xm=':has(.rel:val("source")) > .href' cm15 index /api/right_scripts filter[]="name==rjs:" |sed -e s/\"//g`
 
-mkdir -p rightscripts_backup
-cd rightscripts_backup
+TIMESTAMP=`date "+%Y%m%d%S"`
+mkdir -p rightscripts_backup-"$TIMESTAMP"
+cd rightscripts_backup-"$TIMESTAMP"
+echo "Logging in "$PWD"/backup.log"
+
 for i in $source_href ;
 do
-	script_name=`/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" --x1=.name cm15 show $i`
-	echo "Saving script: $script_name"
-	/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" cm15 show_source $i > "$script_name"
+        script_name=`/usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" --x1=.name cm15 show $i`
+        echo "Saving script: $script_name with URI $i" >> backup.log
+        /usr/local/bin/rsc --pp -a "$ACCOUNT" --email="$EMAIL" --pwd="$PASSWORD" -h "$SHARD" cm15 show_source $i > "$script_name"
 done
 
-echo "Script was saved in rightscripts_backup"
+cd ..
+echo "Script was saved in "$PWD"/rightscripts_backup-"$TIMESTAMP" and backup log in backup.log"
